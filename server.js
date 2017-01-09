0663f035b9f16d7f98e4e178760160af4dac6813
johnstamatakos <jstamatakos@xogrp.com>
2017-01-09 16:59:41 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var keypress = require('keypress');
+var player = require('play-sound')(opts = {});
+
+ 
+// make `process.stdin` begin emitting "keypress" events 
+keypress(process.stdin);
+ 
+// listen for the "keypress" event 
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  } else if (!key || !key.name) {
+    console.log('undefined');
+  } else {
+    console.log('got "keypress"', key.name);
+    mapKeyToSound(key.name);
+  }
+});
+ 
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+
+var mapKeyToSound = function(i) {
+  switch(i) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      player.play('wav/kick.wav');
+      break;
+    case 'space':
+      player.play('wav/snare.wav');
+      break;
+    case 'z':
+    case 'x':
+    case 'n':
+    case 'm':
+      player.play('wav/phazed.wav');
+      break;
+    default: 
+      player.play('wav/tick.wav');
+      break;
+  }
+}
